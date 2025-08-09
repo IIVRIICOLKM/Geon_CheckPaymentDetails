b078bcc903622cb69ce07b0d52d5dfeb504f0c73
이건민 <djrlee1@naver.com>
2025-08-09 17:29:05 +0900

    Feat - 홈 화면 프레임 제작 완료

--- CheckPaymentDetails_FE/lib/Widgets/statefulView.dart
+++ CheckPaymentDetails_FE/lib/Widgets/statefulView.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class TabBarScreen extends StatefulWidget with ChangeNotifier{
+class TabBarScreen extends StatefulWidget{
   TabBarScreen({Key? key}) : super(key: key);
   _TabBarScreenState tabBarScreenState = _TabBarScreenState();
 
@@ -16,17 +16,9 @@ class _TabBarScreenState extends State<TabBarScreen> with TickerProviderStateMix
     length: 3,
     vsync: this,
     initialIndex: 0,
-
     /// 탭 변경 애니메이션 시간
     animationDuration: const Duration(milliseconds: 800),
   );
-
-  void updateIndex(){
-    setState(() {
-      nowIndex = this.tabController.index;
-    });
-    notifyListeners();
-  }
 
   @override
   void initState(){
@@ -41,9 +33,7 @@ class _TabBarScreenState extends State<TabBarScreen> with TickerProviderStateMix
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _tabBar()
-    );
+    return _tabBar();
   }
   // TabController -> controller which shows information of tabbar
   Widget _tabBar() {
@@ -66,7 +56,12 @@ class _TabBarScreenState extends State<TabBarScreen> with TickerProviderStateMix
       indicatorColor: Colors.black,
       indicatorWeight: 5,
       indicatorSize: TabBarIndicatorSize.tab,
-      onTap: (index) => {updateIndex()}
+      onTap: (index) => {
+        setState(() {
+          nowIndex = this.tabController.index;
+          notifyListeners();
+        })
+      }
     );
   }
 }
@@ -80,28 +75,72 @@ class PaymentInformation extends StatefulWidget{
 
 class _PaymentInformationState extends State<PaymentInformation>{
 
+  TabBarScreen tabBarScreen = TabBarScreen();
+
   @override
   void initState(){
     super.initState();
   }
 
-  TabBarScreen tabBarScreen = TabBarScreen();
-
+  // 하나의 build 함수 내부에서 Provider를 소비, 생산을 동시에 하는 경우 Consumer 메서드 사용
+  // 아닌 경우 context.read, watch<T>().Func 패턴 사용
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         tabBarScreen,
-        ChangeNotifierProvider(
-          create: (_) => tabBarScreen.tabBarScreenState,
-          child: Consumer<_TabBarScreenState>(
-            builder: (context, tabbarscreen, child){
-              return Text('${tabbarscreen.nowIndex}');
-            }
-          )
-        ),
+        SizedBox(height: 40),
+        PaymentPresentation(tabBarScreen: tabBarScreen)
       ],
+    );
+  }
+}
+
+class PaymentPresentation extends StatefulWidget{
+
+  TabBarScreen tabBarScreen = TabBarScreen();
+  PaymentPresentation({super.key, required TabBarScreen tabBarScreen}){
+    this.tabBarScreen = tabBarScreen;
+  }
+
+  @override
+  State<PaymentPresentation> createState() => _PaymentPresentationState();
+}
+
+class _PaymentPresentationState extends State<PaymentPresentation>{
+
+  TabBarScreen tabBarScreen = TabBarScreen();
+
+  Widget build(BuildContext context){
+
+    this.tabBarScreen = widget.tabBarScreen;
+
+    return ChangeNotifierProvider(
+      create: (_) => tabBarScreen.tabBarScreenState,
+      child: Consumer<_TabBarScreenState>(
+          builder: (context, tabscr, child){
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                if (tabscr.nowIndex == 0)
+                  Text('2025년 8월 9일의 소비액은 얼마입니다.')
+                else if (tabscr.nowIndex == 1)
+                  Text('2025년 7월 9일 부터 8월 9일 까지의 소비액은 얼마입니다.')
+                else
+                  Text('2024년 8월 9일 부터 2025년 8월 9일 까지의 소비액은 얼마입니다.'),
+                SizedBox(height: 40),
+                Container(
+                  width: 300,
+                  height: 300,
+                  padding: EdgeInsets.all(20),
+                  color: Colors.greenAccent,
+                ),
+                Divider(height: 2, color: Colors.black)
+              ],
+            );
+          }
+      ),
     );
   }
 }
